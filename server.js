de7a75804f2a0204ade3d75da362a1ca8bbd1643
Alber Erre <arblugo@gmail.com>
2018-11-09 19:57:05 +0000

    init Movies API

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const port = 3001;
 app.use(express.json());
 
 const users = [{ name: "Alber", id: 1 }, { name: "Juan", id: 2 }];
+const movies = [
+    { name: "Avatar", year: 2009, genre: "fantasy", id: 1 },
+    { name: "Interstellar", year: 2014, genre: "science", id: 2 }
+];
 
 app.get("/", (req, res) => {
     //console.log("Request:", req);
@@ -42,13 +46,56 @@ app.post("/users", (req, res) => {
     if (utils.bodyIsEmpty(req.body)) {
         res.status(400).send("Oops, you have to pass something as Body");
     } else {
-        const newUser = req.body;
-        newUser.id = Math.round(Math.random() * 1000000 + 1);
-        users.push(newUser);
-        res.json(newUser);
+        const newMovie = req.body;
+        newMovie.id = Math.round(Math.random() * 1000000 + 1);
+        users.push(newMovie);
+        res.json(newMovie);
     }
 });
 
+// CRUD (Movies)
+
+app.get("/movies", (req, res) => {
+    //console.log("Request:", req);
+    res.json(movies);
+});
+
+app.get("/movies/:id", (req, res) => {
+    let movieID = req.params.id;
+    let movie = movies.find(movie => movie.id == movieID);
+    res.json(movie);
+});
+
+app.post("/movies", (req, res) => {
+    if (utils.bodyIsEmpty(req.body)) {
+        res.status(400).send("Oops, you have to pass something as Body");
+    } else {
+        const newMovie = req.body;
+        newMovie.id = Math.round(Math.random() * 1000000 + 1);
+        movies.push(newMovie);
+        res.json(newMovie);
+    }
+});
+
+app.put("/movies/:id", (req, res) => {
+    let movieID = req.params.id;
+    let MovieToUpdate = movies.find(movie => movie.id == movieID);
+
+    // update movie data here
+
+    res.send(`${MovieToUpdate.name} has been updated.`);
+});
+
+app.delete("/movies/:id", (req, res) => {
+    let movieID = req.params.id;
+    let MovieToDelete = movies.find(movie => movie.id == movieID);
+
+    // remove from movies array here (using lodash)
+
+    res.send(`${MovieToDelete.name} has been deleted.`);
+});
+
+// Listen
 app.listen(port, () => {
     console.log("Express running on port 3001.");
 });
