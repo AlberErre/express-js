dc0ca3818306dd3f48e3e0e91935362205af5706
Alber Erre <arblugo@gmail.com>
2018-11-09 17:31:47 +0000

    add user routes

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
+const port = 3001;
+
+const users = [{ name: "Alber", id: 1 }, { name: "Juan", id: 2 }];
 
 app.get("/", (req, res) => {
+    console.log("Request:", req);
     res.send("Hola!");
+    //res.json(users);
 });
 
-app.listen(3000, () => {
-    console.log("Express ready on port 3000.");
+app.get("/users", (req, res) => {
+    console.log("Request:", req);
+    res.json(users);
+});
+
+app.get("/users/:id", (req, res) => {
+    const userID = req.params.id;
+    const user = users.find(user => user.id == userID);
+    res.json(user);
+});
+
+app.listen(port, () => {
+    console.log("Express running on port 3001.");
 });
